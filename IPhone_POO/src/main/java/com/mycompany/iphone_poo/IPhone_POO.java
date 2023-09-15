6fefb920741d1d4b74182c6dfe4fa12d1ca97e0a
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2023-09-15 18:23:54 -0300

    Avançando com codigo POO

--- IPhone_POO/src/main/java/com/mycompany/iphone_poo/IPhone_POO.java
+++ IPhone_POO/src/main/java/com/mycompany/iphone_poo/IPhone_POO.java
@@ -1,6 +1,62 @@
 package com.mycompany.iphone_poo;
+import java.util.Scanner;
+import Main.ReprodutorMusical;
+import Main.AparelhoTelefonico;
+import Main.Navegador;
 public class IPhone_POO {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        Scanner read;
+        read = new Scanner(System.in);
+        ReprodutorMusical ReprodutorMusical = new ReprodutorMusical();
+        AparelhoTelefonico AparelhoTelefonico = new AparelhoTelefonico();
+        Navegador Navegador = new Navegador();
+        
+        
+        int choice = 4;
+        do{
+                System.out.println("Escolha uma opção:");
+                System.out.println("1 - Telefonia");
+                System.out.println("2 - Internet");
+                System.out.println("3 - Musica");
+                System.out.println("0 - Sair");
+                
+                choice = read.nextInt();
+                read.nextLine(); 
+                
+                switch (choice){
+                    case 1:{
+                        do{
+                            System.out.println("Escolha uma opção:");
+                            System.out.println("1 - Ligar");
+                            System.out.println("2 - Atender");
+                            System.out.println("3 - Correio de Vóz");
+                            System.out.println("0 - Sair");
+                            
+                            choice = read.nextInt();
+                            read.nextLine(); 
+
+                            switch(choice){
+                                case 1:{
+                                    AparelhoTelefonico.ligar();
+                                    
+                                }
+                                case 2:{
+                                    AparelhoTelefonico.atender();
+                                        
+                                }
+                                case 3:{
+                                    AparelhoTelefonico.correio();
+                                    
+                                }
+                            }
+                        }while (choice != 0);
+                    }
+                }
+                
+        }while (choice != 0);
+       
+        
+        
+    
+}
 }
